3a9730a05096007d3d0884b46b9a79a010bbf065
qifu1177 <qifu1177@gmail.com>
2018-08-24 14:28:21 +0200

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -7,7 +7,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 lstm_size = 100
 
 
-class rcnn:
+class cnn:
     def __init__(self, width, height, label_size):
         self.width = width
         self.height = height
@@ -18,9 +18,12 @@ class rcnn:
         self.lr = tf.placeholder(tf.float32)
         # self.lr = 0.01
         self.build_netz()
+        # Calculate loss value
         self.loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.labels, logits=self.prediction)
+        # Optimization
         self.train_step = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
         self.test_procent = tf.equal(tf.argmax(self.prediction, 1), tf.argmax(self.labels, 1))
+        # Calculate the accuracy of the test value
         self.test_acc = tf.reduce_mean(tf.cast(self.test_procent, tf.float32))
         self.sess = tf.Session()
         self.sess.run(tf.global_variables_initializer())
@@ -28,6 +31,7 @@ class rcnn:
     def close(self):
         self.sess.close()
 
+    # Training Data
     def train(self, x, y, keep_prob, lr=0.01):
         self.sess.run(self.train_step,
                       feed_dict={self.input_datas: x, self.labels: y, self.keep_prob: keep_prob, self.lr: lr})
@@ -37,71 +41,33 @@ class rcnn:
                             feed_dict={self.input_datas: x, self.labels: y, self.keep_prob: 1.0, self.lr: 1.0})
         return out
 
+    # Predict Data
     def predict(self, x):
         y = np.zeros([1, 10])
         return self.sess.run(self.prediction,
                              feed_dict={self.input_datas: x, self.labels: y, self.keep_prob: 1.0, self.lr: 1.0})
-
+    # Build a neural network
     def build_netz(self):
+        # Change dimension from 1 dimension to 2 dimension
         x = tf.reshape(self.input_datas, [-1, self.height, self.width, 1])
 
+        # Add cnn layer
         cnn1 = self.layer_cnn(x, 3, 3, 1, 32, stride=1, active_fun=tf.nn.relu)
+        # Add max pooling layer
         cnn1_pool = self.layer_maxpool(cnn1, 2, stride=2)
         cnn2 = self.layer_cnn(cnn1_pool, 3, 3, 32, 64, stride=1, active_fun=tf.nn.relu)
         cnn_pool = self.layer_maxpool(cnn2, 2, stride=2)
         w = int(self.width / 4)
         h = int(self.height / 4)
+        # Change dimension from 2 dimension to 1 dimension
         x1 = tf.reshape(cnn_pool, [-1, w * h * 64])
-        #self.prediction = self.layer_fc(x1, w * h * 64, self.labels_size, tf.nn.softmax)
+        # Add a fully connected layer
         x2 = self.layer_fc(x1, w * h * 64, 1000, active_fun=tf.nn.relu)
+        # Prevent overfitting
         x2_keep = tf.nn.dropout(x2, self.keep_prob)
         self.prediction = self.layer_fc(x2_keep, 1000, self.labels_size,active_fun=tf.nn.softmax)
 
-        #self.prediction = tf.layers.dense(x2_keep, self.labels_size, activation=tf.nn.softmax)
-
-    def build_netz2(self):
-        x = tf.reshape(self.input_datas, [-1, self.height, self.width, 1])
-
-        cnn1 = tf.layers.conv2d(x, 32, 3, 1, "SAME", activation=tf.nn.relu)
-        cnn1_pool = tf.layers.max_pooling2d(cnn1, pool_size=2, strides=2, padding="SAME")
-        cnn2 = tf.layers.conv2d(cnn1_pool, 64, 3, 1, "SAME", activation=tf.nn.relu)
-        cnn_pool = tf.layers.max_pooling2d(cnn2, pool_size=2, strides=2, padding="SAME")
-        w = int(self.width / 4)
-        h = int(self.height / 4)
-        x1 = tf.reshape(cnn_pool, [-1, w * h * 64])
-        x2 = tf.layers.dense(x1, 1000, activation=tf.nn.relu)
-        x2_keep = tf.nn.dropout(x2, self.keep_prob)
-        self.prediction = tf.layers.dense(x2_keep, self.labels_size, activation=tf.nn.softmax)
-
-    def build_netz3(self):
-        x = tf.reshape(self.input_datas, [-1, self.height, self.width, 1])
-
-        cnn1 = self.layer_cnn(x, 3, 3, 1, 32, stride=1, active_fun=tf.nn.relu)
-        cnn1_pool = self.layer_maxpool(cnn1, 2, stride=2)
-        cnn2 = self.layer_cnn(cnn1_pool, 3, 3, 32, 64, stride=1, active_fun=tf.nn.relu)
-        cnn_pool = self.layer_maxpool(cnn2, 2, stride=2)
-        w = int(self.width / 4)
-        h = int(self.height / 4)
-        x1 = tf.reshape(cnn_pool, [-1, w * h * 64])
-
-        x2 = self.layer_fc(x1, w * h * 64, n_steps* lstm_size, active_fun=tf.nn.relu)
-        x2_keep = tf.nn.dropout(x2, self.keep_prob)
-        rx = tf.reshape(x2_keep, [-1, n_steps, lstm_size])
-        rnn1 = self.layer_rnn(rx, lstm_size, tf.nn.tanh)
-        self.prediction = self.layer_fc(rnn1, lstm_size, self.labels_size,tf.nn.softmax)
-
-    def build_netz4(self):
-        x = tf.reshape(self.input_datas, [-1, self.height, self.width, 1])
-
-        cnn1 = self.layer_cnn(x, 3, 3, 1, 8, stride=1, active_fun=tf.nn.relu)
-        cnn2 = self.layer_cnn(cnn1, 3, 3, 8, 32, stride=1, active_fun=tf.nn.relu)
-        cnn_pool = self.layer_maxpool(cnn2, 2, stride=2)
-        w = int(self.width / 2)
-        h = int(self.height / 2)
-        x1 = tf.reshape(cnn_pool, [-1, w * h * 32])
-
-        self.prediction = self.layer_fc(x1, w * h * 32, self.labels_size,tf.nn.softmax)
-
+    # Define a fully connected layer
     def layer_fc(self, input_datas, input_size, output_size, active_fun=None):
         w = tf.Variable(tf.truncated_normal([input_size, output_size], stddev=0.1))
         b = tf.Variable(tf.constant(0.1, shape=[output_size]))
@@ -112,6 +78,7 @@ class rcnn:
         else:
             return out
 
+    # Define a cnn layer
     def layer_cnn(self, input_datas, filter_h, fiter_w, input_kernel, output_kernel, stride=1, active_fun=None):
         w = tf.Variable(tf.truncated_normal([filter_h, fiter_w, input_kernel, output_kernel], stddev=0.1))
         b = tf.Variable(tf.constant(0.1, shape=[output_kernel]))
@@ -120,11 +87,11 @@ class rcnn:
             return active_fun(out)
         return out
 
-
+    # Define a max polling layer
     def layer_maxpool(self, input_datas, size, stride=2):
         return tf.nn.max_pool(input_datas, [1, size, size, 1], [1, stride, stride, 1], "SAME")
 
-
+    # Define a rnn layer
     def layer_rnn(self, input_datas, lstm_size, active_fun=None):
         cell = tf.contrib.rnn.BasicLSTMCell(lstm_size)
         out, finalstat = tf.nn.dynamic_rnn(cell, input_datas, dtype=tf.float32)
@@ -148,7 +115,7 @@ if __name__ == "__main__":
     n_inputs = 28
     n_outputs = 10
 
-    netz = rcnn(n_inputs, n_steps, n_outputs)
+    netz = cnn(n_inputs, n_steps, n_outputs)
     for i in range(20):
         for empoch in range(n_batch):
             xs, ys = mnist.train.next_batch(batch_size)
